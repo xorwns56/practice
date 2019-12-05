d610066a9ae574f707c6a0654fa23a0a6c96e22d
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2019-12-05 20:22:43 +0900

    Update LeetCode_53.java

--- LeetCode_53.java
+++ LeetCode_53.java
@@ -8,18 +8,19 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 */
 
-int maxSubArray(int* nums, int numsSize){
-    int table[numsSize];
-    int subArraySize = 1; //SubArray의 크기는 최소 1이므로 1로 초기화
-    int max = -2147483647; //max를 int형의 최솟값으로 초기화
-    
-    while(subArraySize<=numsSize){ //SubArray의 크기가 numsSize 이하일 동안 반복
-        for(int i=0;i<=numsSize-subArraySize;i++){
-            if(subArraySize==1) table[i] = nums[i];
-            else table[i] += nums[i+subArraySize-1];
-            if(max < table[i]) max = table[i]; //max값 갱신
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int table[] = new int[nums.length];
+        int subArraySize = 1; //SubArray의 크기는 최소 1이므로 1로 초기화
+        int max = -2147483647; //max를 int형의 최솟값으로 초기화
+        while(subArraySize<=nums.length){ //SubArray의 크기가 numsSize 이하일 동안 반복
+            for(int i=0;i<=nums.length-subArraySize;i++){
+                if(subArraySize==1) table[i] = nums[i];
+                else table[i] += nums[i+subArraySize-1];
+                if(max < table[i]) max = table[i]; //max값 갱신
+            }
+            subArraySize++;
         }
-        subArraySize++;
-    }
     return max;
+    }
 }
