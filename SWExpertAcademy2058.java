68ab2f6a61a9ea0d4bf15e3de600138ced826760
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2019-12-26 21:22:41 +0900

    Update SWExpertAcademy2058.java

--- SWExpertAcademy2058.java
+++ SWExpertAcademy2058.java
@@ -6,13 +6,13 @@ class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int x;
-        x = sc.nextInt();
+        	x = sc.nextInt();
 		int sum = 0;
-        for(int i=0;i<4;i++){
-        	int rest = x%10;
-            x /= 10;
-            sum += rest;
-        }
-        System.out.println(sum);
+        	for(int i=0;i<4;i++){
+        		int rest = x%10;
+           	 x /= 10;
+            	sum += rest;
+        	}
+        	System.out.println(sum);
 	}
 }
