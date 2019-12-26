fdf0ed90762ae1a5e7f45b9cbb3dccc1e04bf5df
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2019-12-26 21:20:22 +0900

    Create SWExpertAcademy2058.java

---
+++ SWExpertAcademy2058.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int x;
+        x = sc.nextInt();
+		int sum = 0;
+        for(int i=0;i<4;i++){
+        	int rest = x%10;
+            x /= 10;
+            sum += rest;
+        }
+        System.out.println(sum);
+	}
+}
