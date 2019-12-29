fdadc8fd2b643347289fdfc267339a8c99831b86
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2019-12-29 17:11:16 +0900

    Update SWExpertAcademy1986.java

--- SWExpertAcademy1986.java
+++ SWExpertAcademy1986.java
@@ -9,11 +9,10 @@ class Solution
 		int[] sum = new int[T];
 		for(int i=0; i<T; i++)
 		{
-            int n = sc.nextInt();
-            sum[i]=0;
-        	for(int j=1;j<=n;j++)	sum[i]+=j%2==1?j:-j;
-            
-            System.out.println("#"+(i+1)+" "+sum[i]);
+            		int n = sc.nextInt();
+            		sum[i]=0;
+        		for(int j=1;j<=n;j++)	sum[i]+=j%2==1?j:-j;
+            		System.out.println("#"+(i+1)+" "+sum[i]);
 		}
 	}
 }
