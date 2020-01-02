706da2702cb5290a942ac3ce9b819c820028ff60
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-02 22:31:20 +0900

    Create SWExpertAcademy2071.java

---
+++ SWExpertAcademy2071.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i=0; i<T; i++)
+		{
+            int sum = 0;
+            for(int x=0;x<10;x++){
+                sum += sc.nextInt();
+            }
+            int avg = (int)(sum/10.0 + 0.5);
+            System.out.println("#"+(i+1)+" "+avg);
+		}
+	}
+}
