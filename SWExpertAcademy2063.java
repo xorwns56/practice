2f1026f077dd1f4a261f679f53a8f073519646ed
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-04 02:05:37 +0900

    Create SWExpertAcademy2063.java

---
+++ SWExpertAcademy2063.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+    Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int array[] = new int[N];
+    for(int i=0;i<N;i++) array[i] = sc.nextInt();
+    Arrays.sort(array);
+    System.out.println(array[N/2]);
+	}
+}
