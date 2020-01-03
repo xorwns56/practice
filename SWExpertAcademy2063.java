f899604b298d668ee541ffb3202fa6ea7a7ea0e2
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-04 02:06:02 +0900

    Update SWExpertAcademy2063.java

--- SWExpertAcademy2063.java
+++ SWExpertAcademy2063.java
@@ -5,11 +5,11 @@ class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-    Scanner sc = new Scanner(System.in);
+    		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int array[] = new int[N];
-    for(int i=0;i<N;i++) array[i] = sc.nextInt();
-    Arrays.sort(array);
-    System.out.println(array[N/2]);
+    		for(int i=0;i<N;i++) array[i] = sc.nextInt();
+   		Arrays.sort(array);
+    		System.out.println(array[N/2]);
 	}
 }
