9434cdc99661a9ed700de854ea4fcddf25aeec53
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-06 00:50:46 +0900

    Create SWExpertAcademy2050.java

---
+++ SWExpertAcademy2050.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s=sc.next();
+		char[] c = s.toCharArray();
+    for(int i=0;i<c.length;i++){
+        System.out.print(c[i] - 'A' + 1);
+        System.out.print(" ");
+    }
+	}
+}
