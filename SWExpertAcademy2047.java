ee86899a724a4e0c782d69e66be94a6cffcdb711
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-07 19:09:25 +0900

    Update SWExpertAcademy2047.java

--- SWExpertAcademy2047.java
+++ SWExpertAcademy2047.java
@@ -7,9 +7,9 @@ class Solution
 		Scanner sc = new Scanner(System.in);
 		String s=sc.next();
 		char[] c = s.toCharArray();
-        for(int i=0;i<c.length;i++){
-            if('a'<=c[i]&&c[i]<='z') c[i] = (char)(c[i] - 'a' + 'A');
-            System.out.print(c[i]);
-        }
+        	for(int i=0;i<c.length;i++){
+			if('a'<=c[i]&&c[i]<='z') c[i] = (char)(c[i] - 'a' + 'A');
+			System.out.print(c[i]);
+        	}
 	}
 }
