3ebc2df0ab3d053c0a24f7db862d406e1892bf21
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-11 01:43:22 +0900

    Update SWExpertAcademy2027.java

--- SWExpertAcademy2027.java
+++ SWExpertAcademy2027.java
@@ -4,11 +4,11 @@ class Solution
 	{
 		for(int i = 0; i < 5; i++)
 		{
-            for(int j = 0; j < 5; j++)
-            {
-                System.out.print(i==j?"#":"+");
-            }
-            System.out.println();
+		    for(int j = 0; j < 5; j++)
+		    {
+			System.out.print(i==j?"#":"+");
+		    }
+		    System.out.println();
 		}
 	}
 }
