37c077d4d2185d634722e2f1dddd3f33f4b9d138
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-11 01:42:53 +0900

    Create SWExpertAcademy2027.java

---
+++ SWExpertAcademy2027.java
@@ -0,0 +1,14 @@
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		for(int i = 0; i < 5; i++)
+		{
+            for(int j = 0; j < 5; j++)
+            {
+                System.out.print(i==j?"#":"+");
+            }
+            System.out.println();
+		}
+	}
+}
