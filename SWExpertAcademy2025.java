cb0310b3aec9e00b1cad2ffba7c0822ee0be029c
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-12 19:11:27 +0900

    Update SWExpertAcademy2025.java

--- SWExpertAcademy2025.java
+++ SWExpertAcademy2025.java
@@ -6,11 +6,11 @@ class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-        int sum = 0;
+        	int sum = 0;
 		for(int i = 1; i <= x; i++)
 		{
 			sum += i;	
 		}
-        System.out.println(sum);
+        	System.out.println(sum);
 	}
 }
