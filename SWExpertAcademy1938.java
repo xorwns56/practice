13fa5023693fdeba2cef19486d4a47acfe4a392c
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-13 19:47:14 +0900

    Update SWExpertAcademy1938.java

--- SWExpertAcademy1938.java
+++ SWExpertAcademy1938.java
@@ -6,11 +6,11 @@ class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
+		int b = sc.nextInt();
+
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
 	}
 }
