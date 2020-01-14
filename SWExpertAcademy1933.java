47dc49e4f590d5f347f1735e524a0605aaba4ef7
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-14 15:08:05 +0900

    Create SWExpertAcademy1933.java

---
+++ SWExpertAcademy1933.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		for(int i = 1; i <= x; i++)
+		{
+            if(x%i==0) System.out.print(i + " ");
+		}
+	}
+}
