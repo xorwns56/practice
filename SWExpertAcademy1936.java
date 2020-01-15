171793658e7efe093f4d0b654f171fd0250bcfed
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-16 02:48:09 +0900

    Create SWExpertAcademy1936.java

---
+++ SWExpertAcademy1936.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a-b<0){
+            if(b-a == 1) System.out.println("B");
+            else System.out.println("A");
+        }else{
+            if(a-b == 1)System.out.println("A");
+            else System.out.println("B");
+        }
+        
+	}
+}
