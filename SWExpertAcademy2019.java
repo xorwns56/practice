c2a8c203725e0b92b39466851833c4c6a7996697
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-17 21:22:19 +0900

    Update SWExpertAcademy2019.java

--- SWExpertAcademy2019.java
+++ SWExpertAcademy2019.java
@@ -7,8 +7,8 @@ class Solution
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
 		for(int i = 0; i <= x; i++)
-        {
-            System.out.print((int)Math.pow(2, i) + " ");
+        	{
+            		System.out.print((int)Math.pow(2, i) + " ");
 		}
 	}
 }
