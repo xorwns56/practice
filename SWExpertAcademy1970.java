4381aca327780db4d506605e21bbd0ce117900a2
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2020-01-19 22:52:16 +0900

    Update SWExpertAcademy1970.java

--- SWExpertAcademy1970.java
+++ SWExpertAcademy1970.java
@@ -10,17 +10,15 @@ class Solution
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-            int x = sc.nextInt();
-			System.out.println("#" + test_case);
-            for(int i=0; i<money.length;i++)
-            {
-            	System.out.print(x/money[i] + " ");
-                x %= money[i];
-            }
-            System.out.println();
-			
-
+		    int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+		    int x = sc.nextInt();
+				System.out.println("#" + test_case);
+		    for(int i=0; i<money.length;i++)
+		    {
+			System.out.print(x/money[i] + " ");
+			x %= money[i];
+		    }
+		    System.out.println();
 		}
 	}
 }
