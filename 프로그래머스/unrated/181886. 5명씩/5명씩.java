9032ad4e265b95923d55a7296586551170135558
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-09 23:38:36 +0900

    [level unrated] Title: 5명씩, Time: 0.22 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,10 +1,8 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] names) {
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < names.length; i += 5) list.add(names[i]);
-        String[] answer = new String[list.size()];
-        for(int i = 0; i < list.size(); i++) answer[i] = list.get(i);
+        String[] answer = new String[(int)Math.ceil(names.length / 5.)];
+        for(int i = 0; i < answer.length; i++) answer[i] = names[i * 5];
         return answer;
     }
 }
