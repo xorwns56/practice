c20a380395ebfea65292d09cde725b89a23f615a
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-11 17:47:59 +0900

    [level Lv.0] Title: ad 제거하기, Time: 2.56 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr).filter(s -> !s.contains("ad")).toArray(String[]::new);
+    }
+}
