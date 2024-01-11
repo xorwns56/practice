c9f25c199b25244e182c23d5616868de97dc8aee
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-11 21:29:31 +0900

    [level Lv.0] Title: 배열 만들기 3, Time: 0.57 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public Integer[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < intervals.length; i++){
+            for(int j = intervals[i][0]; j <= intervals[i][1]; j++) list.add(arr[j]);
+        }
+        return list.toArray(new Integer[0]);
+    }
+}
