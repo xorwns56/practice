5f8dcf66e0575c2ad99305b07070198e8419934e
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-18 21:56:51 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int hp) { 
+        int answer = hp / 5;
+        hp -= hp / 5 * 5;
+        answer += hp / 3;
+        hp -= hp / 3 * 3;
+        answer += hp;
+        return answer;
+    }
+}
