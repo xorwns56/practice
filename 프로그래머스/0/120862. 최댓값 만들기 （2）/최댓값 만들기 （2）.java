7dbf28a095f6629adb89f0489d44c0350be85d4d
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-22 00:41:21 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                if(max < numbers[i] * numbers[j]) max = numbers[i] * numbers[j];
+            }
+        }
+        
+        return max;
+    }
+}
