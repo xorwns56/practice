cf495e9c1b631f172d3ad204816d474bfedcbf59
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-22 01:14:26 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order > 0){
+            int num = order % 10;
+            if(num != 0 && num % 3 == 0) answer++;
+            order /= 10;
+        }
+        return answer;
+    }
+}
