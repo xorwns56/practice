68f20818c6a8016e020d5f832dde9c9a0ea6939f
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-21 19:23:50 +0900

    [level 0] Title: 대문자와 소문자, Time: 14.93 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,11 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        for(char c : my_string.toCharArray()){
-            if('a' <= c && c <= 'z') answer += (char)(c - 'a' + 'A');
-            else answer += (char)(c - 'A' + 'a');
-            
-        }
+        for(char c : my_string.toCharArray()) answer += (char)(c ^ 32);
         return answer;
     }
 }
