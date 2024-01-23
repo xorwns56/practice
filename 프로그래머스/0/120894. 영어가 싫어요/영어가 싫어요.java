dfe72aa7b6dce0af4c1e69498b2fa8aaa844ba38
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-23 22:55:11 +0900

    [level 0] Title: 영어가 싫어요, Time: 9.06 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String numbers) {
+        HashMap<String, Integer> map = new HashMap<>();
+        String[] numStr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        for(int i = 0; i < numStr.length; i++) map.put(numStr[i], i);
+        String tmp = "";
+        long answer = 0;
+        for(int i = 0; i < numbers.length(); i++){
+            tmp += numbers.charAt(i);
+            if(map.containsKey(tmp)){
+                answer = answer * 10 + map.get(tmp);
+                tmp = "";
+            }
+        }
+        return answer;
+    }
+}
