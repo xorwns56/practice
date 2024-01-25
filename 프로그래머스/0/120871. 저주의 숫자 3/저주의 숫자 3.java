fad50105d7551801c2fe4f2a2791554f75407251
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-26 01:25:39 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int count = 0;
+        while(count < n){
+            answer++;
+            while(three(answer)) answer++;
+            count++;
+        }
+        return answer;
+    }
+    
+    public boolean three(int n){
+        if(n % 3 == 0) return true;
+        while(n > 0){
+            if(n % 10 == 3) return true;
+            n /= 10;
+        }
+        return false;
+    }
+}
