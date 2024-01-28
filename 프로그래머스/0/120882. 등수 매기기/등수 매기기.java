5eaeec3a86c3e5c147a874017063b3bf06c0e69d
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-01-28 23:45:53 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        for(int i = 0; i < answer.length; i++) answer[i] = 1;
+        for(int i = 0; i < score.length; i++){
+            for(int j = 0; j < score.length; j++){
+                if(i == j) continue;
+                if((score[i][0] + score[i][1]) / 2. > (score[j][0] + score[j][1]) / 2.) answer[j]++;
+            }
+        }
+        return answer;
+    }
+}
