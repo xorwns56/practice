2ba663aa0e182b6c509926c67efe363e7c6d80c0
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-11 18:14:57 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i = 0; i < arr.length; i++) answer += arr[i];
+        return answer / arr.length;
+    }
+}
