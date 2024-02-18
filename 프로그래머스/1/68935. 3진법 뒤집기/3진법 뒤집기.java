ecd306999a603f7b3503efd41c27c51492a557fe
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-18 23:01:41 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,9 +1,8 @@
-import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
         while(n > 0){
-            answer = answer * 3 + (n % 3);
+            answer = answer * 3 + n % 3;
             n /= 3;
         }
         return answer;
