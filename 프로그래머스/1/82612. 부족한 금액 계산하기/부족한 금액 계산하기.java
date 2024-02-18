22c4d9f4c36b570d359a9e41ca04ff3f9ec9efdf
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-18 21:25:13 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for(int i = 1; i <= count; i++) answer += price * i;
+        answer = Math.max(0, answer - money);
+        return answer;
+    }
+}
