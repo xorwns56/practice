f08fea18c11edc758edf735a60fe14a121c240a7
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-22 22:21:49 +0900

    [level 1] Title: 옹알이 (2), Time: 2.82 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")) answer++;
+        }
+        return answer;
+    }
+}
