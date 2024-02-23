cfcf1631f4c69114cf5714246fc140c4c5c4046b
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-23 20:55:09 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.33 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id;
+        answer = answer.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9-_.]","");
+        answer = answer.replaceAll("\\.{2,}",".");
+        answer = answer.replaceAll("^\\.|\\.$", "");
+        if(answer.isEmpty()) answer = "a";
+        if(answer.length() >= 16) answer = answer.substring(0, 15);
+        if(answer.endsWith(".")) answer = answer.substring(0, 14);
+        if(answer.length() <= 2){
+            while(answer.length() < 3) answer += answer.charAt(answer.length() - 1);
+        }
+        return answer;
+    }
+}
