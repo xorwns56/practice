76db5b95d1b7be1a9f4c659a7f1459cf4ec2df56
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-25 22:20:28 +0900

    [level 2] Title: 올바른 괄호, Time: 23.19 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == '(' && c == ')') stack.pop();
+            else stack.push(c);
+        }
+        return stack.size() == 0;
+    }
+}
