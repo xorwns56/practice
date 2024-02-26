22be8d634849f028cb1cb042ec5a3d66a330fe4b
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-26 17:26:06 +0900

    [level 2] Title: 예상 대진표, Time: 33.16 ms, Memory: 99.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int[] player = new int[n];
+        for(int i = 0; i < n; i++) player[i] = i + 1;
+        return game_ab(player, a, b, 1);
+    }
+    public int game_ab(int[] player, int a, int b, int round){
+        int[] winner = new int[player.length / 2];
+        //System.out.println("Round " + round);
+        for(int i = 0; i < player.length / 2; i++){
+            //System.out.println(player[i * 2] + ", " + player[i * 2 + 1]);            
+            winner[i] = player[i * 2];
+            if((player[i * 2] == a && player[i * 2 + 1] == b) || (player[i * 2] == b && player[i * 2 + 1] == a)) return round;
+            else if(player[i * 2 + 1] == a || player[i * 2 + 1] == b) winner[i] = player[i * 2 + 1];
+        }
+        return game_ab(winner, a, b, round + 1);
+    }
+
+}
