1b736367758dbb378aa64fc3073fcd1dbd03cf13
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-02-29 23:57:32 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    int answer = 0;
+    public int solution(int k, int[][] dungeons) {
+        dungeon(k, dungeons, new boolean[dungeons.length], 0);
+        return answer;
+    }
+    public void dungeon(int k, int[][] dungeons, boolean[] visit, int visit_count){
+        for(int i = 0; i < dungeons.length; i++){
+            if(dungeons[i][0] <= k && !visit[i]){
+                visit[i] = true;
+                dungeon(k - dungeons[i][1], dungeons, visit, visit_count + 1);
+                visit[i] = false;
+            }
+        }
+        answer = Math.max(answer, visit_count);
+    }
+}
