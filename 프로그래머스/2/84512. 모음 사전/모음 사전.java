fffff790d3caf6a1e777cd951887997c6ce1aa3a
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-03-07 23:34:27 +0900

    [level 2] Title: 모음 사전, Time: 14.96 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    char[] aeiou = new char[] { 'A', 'E', 'I', 'O', 'U' };
+    int count = 0;
+    String word;
+    boolean search = false;
+    public int solution(String word) {
+        this.word = word;
+        dfs("");
+        return count;
+    }
+    public void dfs(String s){
+        if(search || s.length() > 5) return;
+        else if(word.equals(s)){
+            search = true;
+            return;
+        }else{
+            count++;
+            for(int i = 0; i < aeiou.length; i++) dfs(s + aeiou[i]);
+        }
+    }
+}
