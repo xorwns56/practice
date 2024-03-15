98d845ab785aef8f85fdfc59211e2138e753f986
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-03-15 21:54:07 +0900

    [level 2] Title: 주식가격, Time: 24.85 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,16 +4,10 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         int[] answer = new int[prices.length];
         for (int i = 0; i < prices.length; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int idx = stack.pop();
-                answer[idx] = i - idx;
-            }
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) answer[stack.peek()] = i - stack.pop();
             stack.push(i);
         }
-        while (!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = prices.length - idx - 1;
-        }
+        while (!stack.isEmpty()) answer[stack.peek()] = prices.length - stack.pop() - 1;
         return answer;
     }
 }
