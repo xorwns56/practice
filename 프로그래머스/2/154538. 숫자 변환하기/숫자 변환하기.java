4e9c8c46f8d1314ed2615dbde7951f5a14b2a30f
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-03-16 22:41:59 +0900

    [level 2] Title: 숫자 변환하기, Time: 2.44 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int x, int y, int n) {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[y];
+        queue.add(new int[] { x, 0 });
+        while(!queue.isEmpty()){
+            int[] tmp = queue.remove();
+            if(tmp[0] == y) return tmp[1];
+            else if(tmp[0] < y){
+                if(!visited[tmp[0]]) {
+                    visited[tmp[0]] = true;
+                    queue.add(new int[] { tmp[0] + n, tmp[1] + 1 });
+                    queue.add(new int[] { tmp[0] * 2, tmp[1] + 1 });
+                    queue.add(new int[] { tmp[0] * 3, tmp[1] + 1 });
+                }
+            }
+        }
+        return -1;
+    }
+    
+    
+}
