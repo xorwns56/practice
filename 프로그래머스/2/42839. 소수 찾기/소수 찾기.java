70588f40fd2425ebba8be4fb4b14eeda5f343892
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-03-17 23:30:10 +0900

    [level 2] Title: 소수 찾기, Time: 13.66 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    int answer = 0;
+    char[] chars;
+    boolean[] visited;
+    public int solution(String numbers) {
+        chars = numbers.toCharArray();
+        visited = new boolean[chars.length];
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < chars.length; i++){
+            visited[i] = true;
+            dfs(set, Integer.parseInt("" + chars[i]));
+            visited[i] = false;
+        }
+        return answer;
+    }
+    public void dfs(HashSet<Integer> set, int value){
+        if(!set.contains(value)){
+            if(isPrimeNumber(value)) answer++;
+            set.add(value);
+        }
+        for(int i = 0; i < visited.length; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            dfs(set, Integer.parseInt(value + "" + chars[i]));
+            visited[i] = false;
+        }
+    }
+    
+    public boolean isPrimeNumber(int number){
+        if(number <= 1) return false;
+        for(int i = 2; i <= (int)Math.sqrt(number); i++){
+            if(number % i == 0) return false;
+        }
+        return true;
+    }
+}
