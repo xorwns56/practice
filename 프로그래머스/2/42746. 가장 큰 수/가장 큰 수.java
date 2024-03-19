df486d2ce186b9c029a5149c38dd07947efa0142
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-03-19 15:48:37 +0900

    [level 2] Title: 가장 큰 수, Time: 1.73 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] numbers_str = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) numbers_str[i] = Integer.toString(numbers[i]);
+        Arrays.sort(numbers_str, (s1, s2)->{
+            return (s2 + s1).compareTo(s1 + s2);
+        });
+        if(numbers_str[0].equals("0")) return "0";
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < numbers_str.length; i++) stringBuilder.append(numbers_str[i]);
+        return stringBuilder.toString();
+    }
+}
