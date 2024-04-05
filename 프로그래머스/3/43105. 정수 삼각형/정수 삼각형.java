2e1d10136c613a51143b9b16bae9a10dc388a966
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-04-05 19:59:42 +0900

    [level 3] Title: 정수 삼각형, Time: 4.35 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -6,7 +6,6 @@ class Solution {
     public int visit(int[][] triangle, int[][] sum, int level, int index){
         if(level == triangle.length) return 0;
         if(sum[level][index] > 0) return sum[level][index];
-        sum[level][index] = triangle[level][index] + Math.max(visit(triangle, sum, level + 1, index), visit(triangle, sum, level + 1, index + 1));
-        return sum[level][index];
+        return sum[level][index] = triangle[level][index] + Math.max(visit(triangle, sum, level + 1, index), visit(triangle, sum, level + 1, index + 1));
     }
 }
