9c05b0a6f5dfe96376bd6a15a2a6a782eea26c1d
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-04-11 22:58:39 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -4,10 +4,11 @@ class Solution {
         while(storey > 0) {
             int num = storey % 10;
             storey /= 10;
-            if (num >= 5) {
+            if(num < 5) answer += num;
+            else{
                 answer += 10 - num;
-                if(num > 5 || storey % 10 >= 5) storey++;
-            }else if (num < 5) answer += num;
+                if(num > 5 || storey % 10 >= 5) storey++; 
+            }
         }
         return answer;
     }
