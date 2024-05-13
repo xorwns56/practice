bfb8069efa6fa60fa5565546d181e16386e3466e
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-05-13 23:31:09 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int denom = lcm(denom1, denom2);
+        int numer = numer1 * (denom / denom1) + numer2 * (denom / denom2);
+        int more = gcd(denom, numer);
+        return new int[]{numer / more, denom / more};
+    }
+    public int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public int lcm(int a, int b){
+        return a * b / gcd(a, b);
+    }
+}
