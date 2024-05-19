7c4897e5f8702f85d624a610c2fa1acdc51b52f1
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-05-19 23:08:37 +0900

    [level 0] Title: 그림 확대, Time: 16.52 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        for(int i = 0; i < picture.length; i++){
+            String tmp = "";
+            for(int j = 0; j < picture[i].length(); j++)
+                for(int l = 0; l < k; l++) tmp += picture[i].charAt(j);
+            for(int j = 0; j < k; j++) answer[i * k + j] = tmp;
+        }
+        return answer;
+    }
+}
