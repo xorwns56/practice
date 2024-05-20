fbeafaad00aac23de55ff87d6147f9d48e3a0871
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-05-20 23:27:16 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i < todo_list.length; i++){
+            if(!finished[i]) list.add(todo_list[i]);
+        }
+        String[] answer = new String[list.size()];
+        list.toArray(answer);
+        return answer;
+    }
+}
