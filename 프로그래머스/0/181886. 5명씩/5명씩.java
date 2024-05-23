b6a4d10adaec0f73eda8e40cf622941d691bb2e0
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-05-23 23:24:03 +0900

    [level 0] Title: 5명씩, Time: 0.25 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(int)Math.ceil(names.length / 5.)];
+        for(int i = 0; i < answer.length; i++) answer[i] = names[i * 5];
+        return answer;
+    }
+}
