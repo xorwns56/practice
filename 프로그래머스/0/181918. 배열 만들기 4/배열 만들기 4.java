523383380c539fed684b3c582fcf29aa65604b79
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-05-27 23:54:15 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.29 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        while(i < arr.length){
+            if(list.size() == 0) list.add(arr[i++]);
+            else{
+                if(list.get(list.size() - 1) < arr[i]) list.add(arr[i++]);
+                else list.remove(list.size() - 1);
+            }
+        }
+        int[] stk = new int[list.size()];
+        for(i = 0; i < list.size(); i++) stk[i] = list.get(i);
+        return stk;
+    }
+}
