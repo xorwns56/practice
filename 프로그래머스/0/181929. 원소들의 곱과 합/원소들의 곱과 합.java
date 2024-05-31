0aa36ed8ba5e6ac24e20fa1a8ffe37a42155feb0
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-05-31 23:58:01 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int mul = 1;
+        int sum = 0;
+        for(int i = 0; i < num_list.length; i++){
+            mul *= num_list[i];
+            sum += num_list[i];
+        }
+        return mul < sum * sum ? 1 : 0;
+    }
+}
