5b29905f7f7d375315fd90b3e4569511b2c2b33b
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2024-06-02 23:48:39 +0900

    [level 0] Title: 배열 만들기 6, Time: 14.50 ms, Memory: 94.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        for(int i = 0; i < arr.length; i++){
+            if(stk.isEmpty() || stk.peek() != arr[i]) stk.push(arr[i]);
+            else if(stk.peek() == arr[i]) stk.pop();
+        }
+        if(stk.isEmpty()) stk.push(-1);
+        int[] answer = new int[stk.size()];
+        for(int i = answer.length - 1; i >= 0; i--) answer[i] = stk.pop();
+        return answer;
+    }
+}
